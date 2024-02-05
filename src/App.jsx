ffb92278445a2e21cc2ca6b14ee64a2749e231cd
Aditya Rawat <adityarawat2801@gmail.com>
2024-02-05 16:38:12 +0530

    pills

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,92 @@
-
-import { useState, useEffect } from 'react'
-import './App.css'
-
+import { useState, useEffect } from "react";
+import "./App.css";
+import Pill from "./components/pill";
 
 function App() {
-   
   const [searchTerm, setSearchTerm] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [selectedUsers, setSelectedUsers] = useState([]);
 
+  const [selectedUserSet, setSelectedUserSet] = useState(new Set());
 
   // fetch('https://dummyjson.com/users/search?q=John')
 
-  
-
   useEffect(() => {
     const fetchUsers = () => {
-      if(searchTerm.trim() === "") {
-      return ;
+      if (searchTerm.trim() === "") {
+        return;
       }
       fetch(`https://dummyjson.com/users/search?q=${searchTerm}`)
-      .then((res) => res.json())
-      .then((data) => setSuggestions(data))
-      .catch((err) => {
-        console.error(err)
-      })
+        .then((res) => res.json())
+        .then((data) => setSuggestions(data))
+        .catch((err) => {
+          console.error(err);
+        });
     };
     fetchUsers();
-  },[searchTerm]);
+  }, [searchTerm]);
 
-  return(
-  <div className='search-container'> 
-    <div className="user-search-input">
-      {/* pills */}
-      {/* input field with search sugestions */}
-      <div>
-        <input type="text" value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)} 
-        placeholder='search for a user...'/>
-        {/* search suggestions */}
-        <ul className='suggestionList'>{suggestions?.users?.map((user,index) => {
-          return <li key={user.email}>
-            <img src={user.image} alt={`{user.firstName} ${user.lastName}`} />
-            <span>{user.firstName} {user.lastName}</span>
-          </li>
-        })}</ul>
+  const handleSelectUser = (user) => {
+    setSelectedUsers([...selectedUsers, user]);
+    setSelectedUserSet(new Set([...selectedUserSet, user.email]));
+    setSearchTerm("");
+    setSuggestions([]);
+  };
+
+  const handleRemoveUser = (user) => {
+    const updatedUsers = selectedUsers.filter(
+      (selectedUser) => selectedUser.id !== user.id
+    );
+    setSelectedUsers(updatedUsers);
+
+    const updatedEmails = new Set(selectedUserSet);
+    updatedEmails.delete(user.email);
+    setSelectedUserSet(updatedEmails);
+  };
+
+  
+
+  return (
+    <div className="search-container">
+      <div className="user-search-input">
+        {/* pills */}
+        {selectedUsers.map((user) => {
+            return (
+               <Pill key={user.email}
+            image={user.image}
+            text={`${user.firstName} ${user.lastName}`}
+            onClick={() => handleRemoveUser(user)}/>
+            );
+          })}
+      
+        {/* input field with search sugestions */}
+        <div>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="search for a user..."
+          />
+          {/* search suggestions */}
+          <ul className="suggestionList">
+            {suggestions?.users?.map((user, index) => {
+              return !selectedUserSet.has(user.email) ? (
+                <li key={user.email} onClick={() => handleSelectUser(user)}>
+                  <img
+                    src={user.image}
+                    alt={`{user.firstName} ${user.lastName}`}
+                  />
+                  <span>
+                    {user.firstName} {user.lastName}
+                  </span>
+                </li>
+              ) : <></>
+            })}
+          </ul>
+        </div>
       </div>
     </div>
-     </div>
-  )
-  
-} 
+  );
+}
 
-export default App
+export default App;
